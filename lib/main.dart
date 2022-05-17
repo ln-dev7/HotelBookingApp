bdb133a1c91a7f989a08755c60d260045bd4d4c4
ln-dev7 <leonelngoya@gmail.com>
2022-05-17 14:53:18 +0100

    debut des sections

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 // import 'package:google_fonts/google_fonts.dart';
 
 const d_green = Color(0xFF54D3C2);
@@ -16,27 +17,82 @@ class MyApp extends StatelessWidget {
       home: HomePage(),
     );
   }
-} 
+}
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: d_green,
-      appBar: MyAppBar(),
-      body: Container(),
+        backgroundColor: d_green,
+        appBar: MyAppBar(),
+        body: SingleChildScrollView(
+          child: Column(children: [
+            SearchSection(),
+            HotelSection(),
+          ]),
+        ));
+  }
+}
+
+// Create a class that extends the AppBar
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        icon: Icon(
+          Icons.arrow_back,
+          color: Colors.grey[800],
+          size: 20,
+        ),
+        onPressed: null,
+      ),
+      title: Text('Explore',
+          style: GoogleFonts.nunito(
+              color: Colors.black, fontSize: 22, fontWeight: FontWeight.w800)),
+      actions: [
+        IconButton(
+          icon: Icon(
+            Icons.favorite_outline_rounded,
+            color: Colors.grey[800],
+            size: 20,
+          ),
+          onPressed: null,
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.place,
+            color: Colors.grey[800],
+            size: 20,
+          ),
+          onPressed: null,
+        ),
+      ],
+      centerTitle: true, // permet de forcer le centrage du texte ( Explore )
+      backgroundColor: Colors.white,
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
+
+class SearchSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      color: Colors.blue,
     );
   }
 }
 
-// Pour creer les AppBar
-class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
-  Size get PreferredSize => new Size.fromHeight(50);
+class HotelSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      title: Text('Home'),
-      backgroundColor: Colors.white,
+    return Container(
+      height: 1500,
+      color: Colors.red,
     );
   }
 }
